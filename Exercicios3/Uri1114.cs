ae0e7aff3cc90bc082016202fc94fee33cbe2dbb
Vinicius Belloli <vbelloli@outlook.com>
2021-01-06 19:50:06 -0300

    Exercícios de fixação 3 C#

---
+++ Exercicios3/Uri1114.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicios3
+{
+   class Uri1114
+   {
+      static void Main(string[] args)
+      {
+         String senha = Console.ReadLine();
+
+         while (senha != "2002")
+         {
+            Console.WriteLine("Senha Invalida");
+            senha = Console.ReadLine();
+         }
+
+         Console.WriteLine("Acesso Permitido");
+      }
+   }
+}
---
+++ Uri1134/Uri1134.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Uri1134
+{
+   class Uri1134
+   {
+      static void Main(string[] args)
+      {
+         int entrada = 0, gasolina = 0, alcool = 0, diesel = 0;
+
+         while (entrada != 4)
+         {
+            entrada = int.Parse(Console.ReadLine());
+
+            if (entrada == 1)
+            {
+               alcool++;
+            }
+            else if (entrada == 2)
+            {
+               gasolina++;
+            }
+            else if (entrada == 3)
+            {
+               diesel++;
+            }
+         }
+
+         Console.WriteLine("MUITO OBRIGADO");
+         Console.WriteLine("Alcool: " + alcool);
+         Console.WriteLine("Gasolina: " + gasolina);
+         Console.WriteLine("Diesel: " + diesel);
+      }
+   }
+}
